5195c8eda1abeb77df65ac380529036d577a80cf
Junghyeon0710 <sie08357@naver.com>
2025-07-02 13:31:49 +0900

    캐릭터 파라곤 #2

---
+++ Source/Crunch/Player/CPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Crunch/Character/CCharacter.h"
+#include "CPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ACPlayerCharacter : public ACCharacter
+{
+	GENERATED_BODY()
+	
+};
