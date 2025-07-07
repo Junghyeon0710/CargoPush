b911df1f51174be201f692f179dc0617e5c89344
Junghyeon0710 <sie08357@naver.com>
2025-07-07 11:25:02 +0900

    GameAbility Default Setting

--- Source/Crunch/Character/CCharacter.h
+++ Source/Crunch/Character/CCharacter.h
@@ -3,17 +3,22 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
 #include "GameFramework/Character.h"
 #include "CCharacter.generated.h"
 
+class UCAttributeSet;
+class UCAbilitySystemComponent;
+
 UCLASS()
-class CRUNCH_API ACCharacter : public ACharacter
+class CRUNCH_API ACCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	ACCharacter();
-
+	void ServerSideInit();
+	void ClientSideInit();
 protected:
 	virtual void BeginPlay() override;
 
@@ -21,5 +26,21 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	/********************************************************/
+	/*					/** Gameplay Ability *		        */
+	/********************************************************/
+	
+	/** Gameplay Ability */
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	/* ~Gameplay Ability **/
+	
+private:
+	UPROPERTY(visibleDefaultsOnly, Category="Gameplay Ability")
+	TObjectPtr<UCAbilitySystemComponent> CAbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UCAttributeSet> CAttributeSet;
 
 };
