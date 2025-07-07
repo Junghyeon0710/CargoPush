770786a9400bf37fbb136f3c77bdb646dca63254
Junghyeon0710 <sie08357@naver.com>
2025-07-07 11:53:39 +0900

    feat: 게이지 위젯 추가

---
+++ Source/Crunch/Widgets/ValueGauge.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ValueGauge.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+
+void UValueGauge::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	ProgressBar->SetFillColorAndOpacity(BarColor);
+	
+}
+
+void UValueGauge::SetValue(float NewValue, float NewMaxValue)
+{
+	if (NewMaxValue == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Value is zero"));
+		return;
+	}
+
+	float NewPercent = NewValue / NewMaxValue;
+	ProgressBar->SetPercent(NewPercent);
+
+	FNumberFormattingOptions FormatOps = FNumberFormattingOptions().SetMaximumFractionalDigits(0);
+
+	ValueText->SetText(
+		FText::Format(
+			FTextFormat::FromString("{0}/{1}"),
+			FText::AsNumber(NewValue, &FormatOps),
+			FText::AsNumber(NewMaxValue, &FormatOps)
+			)
+		);
+}
