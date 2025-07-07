78010e485369d09a3f1c3820e17391229f922b49
Junghyeon0710 <sie08357@naver.com>
2025-07-07 22:35:18 +0900

    feat: 콤보 어빌리티 클래스 추가

---
+++ Source/Crunch/GAS/CGameplayAbility.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CGameplayAbility.h"
+
+void UCGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("UGA_Combo::ActivateAbility"));
+}
