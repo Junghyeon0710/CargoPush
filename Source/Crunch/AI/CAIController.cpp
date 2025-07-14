6c840fa4da3d398a077d9227014b9ac5db76f591
Junghyeon0710 <sie08357@naver.com>
2025-07-14 09:12:55 +0900

    feat: AI추가, 시각인식 및 팀 ID 셋팅

---
+++ Source/Crunch/AI/CAIController.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CAIController.h"
+
+#include "Crunch/Character/CCharacter.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+
+
+ACAIController::ACAIController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>("AI Perception Component");
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>("Sight Config");
+
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = false;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = false;
+
+	SightConfig->SightRadius = 1000.f;
+	SightConfig->LoseSightRadius = 1200.f;
+
+	SightConfig->SetMaxAge(5.f);
+
+	SightConfig->PeripheralVisionAngleDegrees = 180.f;
+
+	AIPerceptionComponent->ConfigureSense(*SightConfig);
+}
+
+void ACAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	SetGenericTeamId(FGenericTeamId(0));
+
+	IGenericTeamAgentInterface* PawnTeamInterface = Cast<IGenericTeamAgentInterface>(InPawn);
+	if (PawnTeamInterface)
+	{
+		PawnTeamInterface->SetGenericTeamId(GetGenericTeamId());	
+	}
+}
+
---
+++ Source/Crunch/AI/CAIController.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "CAIController.generated.h"
+
+class UAISenseConfig_Sight;
+class UAIPerceptionComponent;
+
+UCLASS()
+class CRUNCH_API ACAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ACAIController();
+
+	virtual void OnPossess(APawn* InPawn) override;
+
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category="Perception")
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Perception")
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
+};
