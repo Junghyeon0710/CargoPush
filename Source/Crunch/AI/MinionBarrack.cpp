158ad4132dcac5e28a2b57bca07215cda870cc31
Junghyeon0710 <sie08357@naver.com>
2025-07-14 22:30:29 +0900

    feat: 미니언 스폰 폴링 시스템 개발

---
+++ Source/Crunch/AI/MinionBarrack.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MinionBarrack.h"
+
+#include "Minion.h"
+#include "GameFramework/PlayerStart.h"
+
+
+AMinionBarrack::AMinionBarrack()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMinionBarrack::BeginPlay()
+{
+	Super::BeginPlay();
+	SpawnNewMinions(5);
+}
+
+void AMinionBarrack::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+const APlayerStart* AMinionBarrack::GetNextSpawnSpot()
+{
+	if (SpawnSpots.Num() == 0)
+	{
+		return nullptr;
+	}
+
+	++NextSpawnSpotIndex;
+
+	if (NextSpawnSpotIndex >= SpawnSpots.Num())
+	{
+		NextSpawnSpotIndex = 0;
+	}
+
+	return SpawnSpots[NextSpawnSpotIndex];
+}
+
+void AMinionBarrack::SpawnNewMinions(int Amount)
+{
+	for (int i = 0; i < Amount; ++i)
+	{
+		FTransform SpawnTransform = GetActorTransform();
+		if (const APlayerStart* NextSpawnSPot = GetNextSpawnSpot())
+		{
+			SpawnTransform = NextSpawnSPot->GetActorTransform();
+		}
+
+		AMinion* NewMinion = GetWorld()->SpawnActorDeferred<AMinion>(MinionClass, SpawnTransform, this, nullptr, ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn);
+		NewMinion->SetGenericTeamId(BarrackTeamId);
+		NewMinion->FinishSpawning(SpawnTransform);
+		MinionPool.Add(NewMinion);
+	}
+}
+
---
+++ Source/Crunch/AI/MinionBarrack.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GenericTeamAgentInterface.h"
+#include "GameFramework/Actor.h"
+#include "MinionBarrack.generated.h"
+
+class AMinion;
+class APlayerStart;
+
+UCLASS()
+class CRUNCH_API AMinionBarrack : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AMinionBarrack();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	FGenericTeamId BarrackTeamId;
+
+	UPROPERTY()
+	TArray<AMinion*> MinionPool;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	TSubclassOf<AMinion> MinionClass;
+
+	UPROPERTY(EditAnywhere, Category = "Spawn")
+	TArray<APlayerStart*> SpawnSpots;
+
+	int32 NextSpawnSpotIndex = -1;
+
+	const APlayerStart* GetNextSpawnSpot();
+
+	void SpawnNewMinions(int Amount);
+};
