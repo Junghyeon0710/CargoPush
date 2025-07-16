cd9e7a3cdf91cf3c9efa46d65f12204326c6bd0b
Junghyeon0710 <sie08357@naver.com>
2025-07-16 14:42:32 +0900

    feat: 어퍼컷 기능 및 키 바인딩

---
+++ Source/Crunch/GAS/UpperCut.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UpperCut.h"
+
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
+void UUpperCut::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (!K2_CommitAbility())
+	{
+		K2_EndAbility()
+		RETURN_IF_EXIT_REQUESTED;
+	}
+
+	if (HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+	{
+		UAbilityTask_PlayMontageAndWait* PlayUpperCutMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, UpperCutMontage);
+		PlayUpperCutMontageTask->OnBlendOut.AddDynamic(this, &ThisClass::K2_EndAbility);
+		PlayUpperCutMontageTask->OnCancelled.AddDynamic(this, &ThisClass::K2_EndAbility);
+		PlayUpperCutMontageTask->OnInterrupted.AddDynamic(this, &ThisClass::K2_EndAbility);
+		PlayUpperCutMontageTask->OnCompleted.AddDynamic(this, &ThisClass::K2_EndAbility);
+		PlayUpperCutMontageTask->ReadyForActivation();
+		
+	}
+}
