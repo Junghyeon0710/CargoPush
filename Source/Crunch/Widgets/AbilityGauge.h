c4259ccda1dcd6bdc1e0bc1206ffdcc14cb9ae5f
Junghyeon0710 <sie08357@naver.com>
2025-07-17 22:01:02 +0900

    feat: 어빌리티 게이지 위젯 생성

---
+++ Source/Crunch/Widgets/AbilityGauge.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AbilityGauge.generated.h"
+
+class UTextBlock;
+class UImage;
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API UAbilityGauge : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> CooldownCounterText;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> CooldownDurationText;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> CostText;
+};
