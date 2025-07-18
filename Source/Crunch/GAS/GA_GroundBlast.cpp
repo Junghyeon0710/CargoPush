9172f8063c320e6ded43556d2ee411e283d7e2d9
Junghyeon0710 <sie08357@naver.com>
2025-07-18 16:31:13 +0900

    feat: 땅 지면 어택 어빌리티 추가

---
+++ Source/Crunch/GAS/GA_GroundBlast.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GA_GroundBlast.h"
+
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
+void UGA_GroundBlast::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	if (!HasAuthorityOrPredictionKey(CurrentActorInfo, &CurrentActivationInfo))
+	{
+		return;
+	}
+
+	UAbilityTask_PlayMontageAndWait* PlayGroundBlastAnimTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, NAME_None, GroundBlastMontage);
+	PlayGroundBlastAnimTask->OnBlendOut.AddDynamic(this, &ThisClass::K2_EndAbility);
+	PlayGroundBlastAnimTask->OnCancelled.AddDynamic(this, &ThisClass::K2_EndAbility);
+	PlayGroundBlastAnimTask->OnInterrupted.AddDynamic(this, &ThisClass::K2_EndAbility);
+	PlayGroundBlastAnimTask->OnCompleted.AddDynamic(this, &ThisClass::K2_EndAbility);
+	PlayGroundBlastAnimTask->ReadyForActivation();
+}
