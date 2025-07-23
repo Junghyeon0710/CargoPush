53c4e656501be776f19abd8a191486df73d55b1a
Junghyeon0710 <sie08357@naver.com>
2025-07-23 17:52:20 +0900

    feat: 죽을시 보상 타겟 설정

--- Source/Crunch/GAS/GAP_Dead.cpp
+++ Source/Crunch/GAS/GAP_Dead.cpp
@@ -4,6 +4,7 @@
 #include "GAP_Dead.h"
 
 #include "UCAbilitySystemStatics.h"
+#include "Engine/OverlapResult.h"
 
 UGAP_Dead::UGAP_Dead()
 {
@@ -25,5 +26,49 @@ void UGAP_Dead::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const F
 		{
 			UE_LOG(LogTemp, Warning, TEXT("Killer : %s"), *Killer->GetName())
 		}
+
+		TArray<AActor*> RewardTargets = GetRewardTargets();
+		for (const AActor* RewardTarget : RewardTargets)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Find Reward Tartg: %s"), *RewardTarget->GetName());
+		}
 	}
+}
+
+TArray<AActor*> UGAP_Dead::GetRewardTargets() const
+{
+	TSet<AActor*> OutActors;
+
+	AActor* AvatarActor = GetAvatarActorFromActorInfo();
+	if (!AvatarActor || !GetWorld())
+	{
+		return OutActors.Array();
+	}
+
+	FCollisionObjectQueryParams CollisionObjectParams;
+	CollisionObjectParams.AddObjectTypesToQuery(ECC_Pawn);
+	FCollisionShape CollisionShape;
+	CollisionShape.SetSphere(RewardRange);
+	
+	TArray<FOverlapResult> OverlapResults;
+	if (GetWorld()->OverlapMultiByObjectType(OverlapResults, AvatarActor->GetActorLocation(), FQuat::Identity, CollisionObjectParams, CollisionShape))
+	{
+		for (FOverlapResult& OverlapResult : OverlapResults)
+		{
+			const IGenericTeamAgentInterface* OtherTeamInterface = Cast<IGenericTeamAgentInterface>(OverlapResult.GetActor());
+			if (!OtherTeamInterface || OtherTeamInterface->GetTeamAttitudeTowards(*AvatarActor) != ETeamAttitude::Hostile)
+			{
+				continue;
+			}
+
+			if (!UCAbilitySystemStatics::IsHero(OverlapResult.GetActor()))
+			{
+				continue;
+			}
+
+			OutActors.Add(OverlapResult.GetActor());
+		}
+	}
+
+	return OutActors.Array();
 }
--- Source/Crunch/GAS/UCAbilitySystemStatics.cpp
+++ Source/Crunch/GAS/UCAbilitySystemStatics.cpp
@@ -3,6 +3,9 @@
 
 #include "UCAbilitySystemStatics.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
 #include "Abilities/GameplayAbility.h"
 
 FGameplayTag UCAbilitySystemStatics::GetBasicAttackAbilityTag()
@@ -54,6 +57,26 @@ FGameplayTag UCAbilitySystemStatics::GetManaFullStatTag()
 FGameplayTag UCAbilitySystemStatics::GetManaEmptyStatTag()
 {
 	return FGameplayTag::RequestGameplayTag("Stats.Mana.Empty");
+}
+
+FGameplayTag UCAbilitySystemStatics::GetHeroRoleTag()
+{
+	return FGameplayTag::RequestGameplayTag("Role.Hero");
+}
+
+bool UCAbilitySystemStatics::IsHero(const AActor* ActorToCheck)
+{
+	const IAbilitySystemInterface* ActorISA = Cast<IAbilitySystemInterface>(ActorToCheck);
+	if (ActorISA)
+	{
+		UAbilitySystemComponent* ActorASC = ActorISA->GetAbilitySystemComponent();
+		if (ActorASC)
+		{
+			return ActorASC->HasMatchingGameplayTag(GetHeroRoleTag());
+		}
+	}
+
+	return false;
 }
 
 float UCAbilitySystemStatics::GetStaticCooldownDurationForAbility(const UGameplayAbility* Ability)
