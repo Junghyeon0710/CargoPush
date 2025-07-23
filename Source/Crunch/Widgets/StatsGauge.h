e31e6d42f28a149865fb25a5bcba584f2b7f53d6
Junghyeon0710 <sie08357@naver.com>
2025-07-23 11:51:32 +0900

    feat: 스텟 게이지 위젯 클래스 생성

---
+++ Source/Crunch/Widgets/StatsGauge.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "Blueprint/UserWidget.h"
+#include "StatsGauge.generated.h"
+
+struct FOnAttributeChangeData;
+class UTextBlock;
+class UImage;
+
+UCLASS()
+class CRUNCH_API UStatsGauge : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativePreConstruct() override;
+	virtual void NativeConstruct() override;
+
+private:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Icon;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> AttributeText;
+
+	UPROPERTY(EditAnywhere, Category = "Visual")
+	UTexture2D* IconTexture;
+
+	UPROPERTY(EditAnywhere, Category = "Attribute")
+	FGameplayAttribute Attribute;
+
+	void SetValue(float NewVal);
+
+	FNumberFormattingOptions NumberFormattingOptions;
+
+	void AttributeChanged(const FOnAttributeChangeData& Data);
+};
