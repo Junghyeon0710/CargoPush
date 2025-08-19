b98cac6bdcdc7031f6055b39af93996fb2c58150
Junghyeon0710 <sie08357@naver.com>
2025-08-19 11:06:05 +0900

    feat: 스톰코어 팀 많은쪽으로 이동

--- Source/Crunch/Framework/StormCore.h
+++ Source/Crunch/Framework/StormCore.h
@@ -12,21 +12,20 @@ class CRUNCH_API AStormCore : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AStormCore();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	virtual void PossessedBy(AController* NewController) override;
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
+	UPROPERTY(EditDefaultsOnly, Category = "Move")
+	float MaxMoveSpeed = 500.f;
+	
 	UPROPERTY(VisibleDefaultsOnly, Category = "Detection")
 	class USphereComponent* InfluenceRange;
 
@@ -37,9 +36,19 @@ private:
 	void InfluencerLeftRange(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 	void UpdateTeamWeight();
+	void UpdateGoal();
+
+	UPROPERTY(EditAnywhere, Category = "Team")
+	AActor* TeamOneGoal;
+
+	UPROPERTY(EditAnywhere, Category = "Team")
+	AActor* TeamTwoGoal;
 
 	int TeamOneInfluncerCount = 0;
 	int TeamTwoInfluncerCount = 0;
 
 	float TeamWeight = 0.f;
+
+	UPROPERTY()
+	class AAIController* OwnerAIC;
 };
