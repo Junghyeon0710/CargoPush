8dbc5c99934ab1a307dc98ef847230f3c6ac9b86
Junghyeon0710 <sie08357@naver.com>
2025-08-22 14:56:32 +0900

    feat: 타겟 액터 구성

--- Source/Crunch/GAS/TargetActor_Line.h
+++ Source/Crunch/GAS/TargetActor_Line.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GenericTeamAgentInterface.h"
 #include "Abilities/GameplayAbilityTargetActor.h"
 #include "TargetActor_Line.generated.h"
 
@@ -10,12 +11,43 @@
  * 
  */
 UCLASS()
-class CRUNCH_API ATargetActor_Line : public AGameplayAbilityTargetActor
+class CRUNCH_API ATargetActor_Line : public AGameplayAbilityTargetActor, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
 
 	ATargetActor_Line();
+	void ConfigureTargetSetting(
+			float NewTargetRange,
+			float NewDetectionCylinderRadius,
+			float NewTargetingInterval,
+			FGenericTeamId OwnerTeamId,
+			bool bShouldDrawDebug
+		);
+
+	virtual void SetGenericTeamId(const FGenericTeamId& NewTeamID) override;
+	
+	/** Retrieve team identifier in form of FGenericTeamId */
+	FORCEINLINE virtual FGenericTeamId GetGenericTeamId() const override { return TeamId; }
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 private:
+	
+	UPROPERTY(Replicated)
+	float TargetRange;
+
+	UPROPERTY(Replicated)
+	float DetectionCylinderRadius;
+
+	UPROPERTY()
+	float TargetingInterval;
+
+	UPROPERTY(Replicated)
+	FGenericTeamId TeamId;
+
+	UPROPERTY()
+	bool bDrawDebug;
+
+	UPROPERTY(Replicated)
+	const AActor* AvatarActor;
 	UPROPERTY(VisibleDefaultsOnly, Category = "Component")
 	class USceneComponent* RootComp;
 
@@ -25,5 +57,4 @@ private:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Component")
 	class USphereComponent* TargetEndDetectionSphere;
 
-	const AActor* AvatarActor;
 };
