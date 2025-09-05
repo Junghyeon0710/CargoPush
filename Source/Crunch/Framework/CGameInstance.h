8aba4a5a531b407428cb4532a4be278a7593414c
안중현 <133496610+Junghyeon0710@users.noreply.github.com>
2025-09-05 16:19:33 +0900

    Feat/server coodinator (#35)
    * feat: 깃 이그노어 파이선 추가
    
    * feat: 세션 체크 타이머 추가
    
    * feat: 세션 검색 중지 및 세션 이름 및 포트 검색
    
    * feat: 서버 연결 구현
    
    * feat: 글로벌 세션 검색 구현
    
    * feat: Session Entry 위젯 추가
    
    * feat: 세션 리스트 채우기
    
    * feat: 게임 접속 기능 구현
    
    * feat: 서버 클라 연결

--- Source/Crunch/Framework/CGameInstance.h
+++ Source/Crunch/Framework/CGameInstance.h
@@ -6,9 +6,13 @@
 #include "Engine/GameInstance.h"
 #include "Interfaces/IHttpResponse.h"
 #include "Interfaces/IHttpRequest.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "CGameInstance.generated.h"
 
 DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnLoginCompleted, bool /*bWasSuccessful*/, const FString& /*PlayerNickName*/, const FString& /*ErrorMsg*/);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnGlobalSessionSearchCompleted, const TArray<FOnlineSessionSearchResult>& /*SearchResults*/)
+DECLARE_MULTICAST_DELEGATE(FOnJoinSesisonFailed);
 
 UCLASS()
 class CRUNCH_API UCGameInstance : public UGameInstance
@@ -38,10 +42,40 @@ private:
 public:
 	void RequestCreateAndJoinSession(const FName& NewSessionName);
 	void CancelSessionCreation();
-	
+	void StartGlobalSessionSearch();
+	bool JoinSessionWithId(const FString& SessionIdStr);
+
+	FOnJoinSesisonFailed OnJoinSessionFailed;
+	FOnGlobalSessionSearchCompleted OnGlobalSessionSearchCompleted;
 private:
 	void SessionCreationRequestCompleted(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bConnectedSuccessfully, FGuid SesisonSearchId);
+	void StartFindingCreatedSession(const FGuid& SessionSearchId);
+	void StopAllSessionFindings();
+	void StopFindingCreatedSession();
+	void StopGlobalSessionSearch();
+	void FindGlobalSessions();
+	void GlobalSessionSearchCompleted(bool bWasSuccessful);
+	
+	FTimerHandle FindCreatedSesisonTimerHandle;
+	FTimerHandle FindCreatedSesisonTimeoutTimerHanle;
+	FTimerHandle GlobalSessionSearchTimerHandle;
 
+	UPROPERTY(EditDefaultsOnly, Category = "Session Search")
+	float GlobalSessionSearchInterval = 2.f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Session Search")
+	float FindCreatedSessionSearchInterval = 1.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Session Search")
+	float FindCreatedSessionTimeoutDuration = 60.f;
+
+	void FindCreatedSession(FGuid SessionSearchId);
+	void FindCreatedSessionTimeout();
+	void FindCreateSessionCompleted(bool bWasSuccessful);
+	void JoinSessionWithSearchResult(const class FOnlineSessionSearchResult& SearchResult);
+	void JoinSessionCompleted(FName SessionName, EOnJoinSessionCompleteResult::Type JoinResult, int Port);
+
+	TSharedPtr<class FOnlineSessionSearch> SessionSearch;
 	/********************************/
 	/*          Session Server      */
 	/********************************/
